cd45f17127e8f1de34ff4adaea64b3e710354d3e
Alan Campbell <alanjamescampbell1@gmail.com>
2020-06-21 11:53:46 +0100

    Update
    MongoDB NodeJS study. Introduction with basic commands

---
+++ NodeJS MongoDB/Files/sort_By_Ascending.js
@@ -0,0 +1,16 @@
+var mongoClient = require('mongodb').MongoClient;
+var url 	= "mongodb://localhost:27017/";
+
+mongoClient.connect(url, function(err, db){
+
+	if(err) throw err;
+	var dbo = db.db("mydb");
+	/** Sort by name	**/
+	var mysort = { name: 1 };
+	dbo.collection("customers").find().sort(mysort).toArray(function(err, result){
+
+		if(err) throw err;
+		console.log(result);
+		db.close();
+	});
+}); 
